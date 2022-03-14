fa118de4e1df03ef735b704a158985c04770f836
Grzegorz Wojdyga <g.wojdyga@gmail.com>
2022-03-14 17:09:55 +0100

    pierwsza trasza

---
+++ collections_loops.py
@@ -0,0 +1,161 @@
+def task1(s):
+    """
+    Function which receives a sequence of comma-separated numbers and generate a list and a tuple which contains
+    every number
+
+    Input: string with comma separated numbers
+    Output: list and tuple with all the numbers
+    """
+    pass
+
+
+def task2():
+    """
+    Function which receives a sequence of comma-separated numbers and generate a result of their addition and
+    multiplication
+
+    Input: string with comma separated numbers
+    Output: two integers
+    """
+    pass
+
+
+def task3():
+    """
+    Function which receives a sequence of comma-separated numbers from console and generate a result of their
+    addition and multiplication
+
+    Input: None
+    Output: two integers
+    """
+    pass
+
+
+def task4():
+    """
+    Function which will print and find all such numbers which are divisible by 7 but are not a multiple of 5,
+    between from 0 to N (argument), both included. 
+
+    Input: Integer N>0 
+    Output: List of all the integers
+    """
+    pass
+
+
+def task5():
+    """
+    Function which will check if argument is integer, if yes print and find all such numbers which are divisible by 7
+    but are not a multiple of 5, between from 0 to N (argument), both included. If not it will print error ant return
+    empty list.
+
+    Input: Integer N>0 
+    Output: List of all the integers
+    """
+    pass
+
+
+def task6():
+    """
+    Function which will check if arguments are integer, if yes print and find all such numbers which are divisible by
+    7 but are not a multiple of 5, between from N to M (arguments), both included. If not it will print error ant
+    return empty list. 
+    
+    Input: Integer M>N>0 
+    Output: List of all the integers
+    """
+    pass
+
+
+def task7(N):
+    """
+    Function which will calculate factorial of a given numbers (N!)
+
+    Input: Integer N>0
+    Output: N!
+    """
+    pass
+    
+
+
+def task8():
+    """
+    Function which will calculate factorial of a given numbers (N!)
+    Output should be string in "aa" notation (e.g. 10.000.000), created by yourself without any string functions.
+
+    Input: Integer N>0
+    Output: N! in aa notation
+    """
+    pass
+
+
+def task9():
+    """
+    Function which will calculate factorial of a given numbers (N!)
+    Output should be rounded to milion in milions printed (e.g. "12 milions")
+
+    Input: Integer N>0
+    Output: N! in milions
+    """
+    pass
+
+
+def task10():
+    """
+    Function which generates a dictionary that contains (i, i*i) such that i is an integral number between 1 and n (
+    both included) e.g. {1: 1, 2: 4, 3: 9, .... } and print it - in every line there must by key and value.
+
+    Input: Integer N>0
+    Output: Dictionary
+    """
+    pass
+
+
+def task11():
+    """
+    Function that print and puts into list all sentences where subject is in list_1 and verb is in list_2 and the object is in list_3
+
+    Input: three lists (e.g. ["I", "You"], ["like", "dislike"], ["Basketball", "Football", "Volleyball"])
+    Output: list of sentences
+    """
+
+
+def task12(n: int) -> int:
+    """
+    Create function that returns n-th element of fibonacci chain
+
+    Input: Single i nteger
+    Output: Integer
+    """
+    pass
+
+
+def task13():
+    """
+    Function that find all such numbers between N and M (both included) such that each digit of the number is
+    an even number.
+    Input: Two integers
+    Output: List of numbers
+    """
+    pass
+
+
+def task14(a, b):
+    """
+    Function that takes 2 numbers, I,J as input and generates a 2-dimensional array. The element value in the i-th
+    row and j-th column of the array should be i*j 
+    
+    Input: string 
+    Output: string
+    """
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    num = 29
+    result = task12(num)
+    #print(result)
+    a, b = 7, 8
+    task14(a, b)
